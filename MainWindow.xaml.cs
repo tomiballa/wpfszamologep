5b143b880a16e92cf4c8b2d5c3e55d5d46a79dab
tomiballa <113887289+tomiballa@users.noreply.github.com>
2023-01-29 23:56:52 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace Szamologep
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Tetszoleges(object sender, RoutedEventArgs e) {
+          String eredmeny = $"{txtAszam.Text} + {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) + Convert.ToDouble(txtBszam.Text)}";
+          MessageBox.Show("Az összeadás eredménye:" + eredmeny);
+          lbEredmenyek.Items.Add(eredmeny);
+        }
+
+        private void btnKivon_Click(object sender, RoutedEventArgs e) {
+          String eredmeny = $"{txtAszam.Text} - {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) - Convert.ToDouble(txtBszam.Text)}";
+          MessageBox.Show("A kivonás eredménye:" + eredmeny);
+          lbEredmenyek.Items.Add(eredmeny);
+        }
+
+        private void btnSzoroz_Click(object sender, RoutedEventArgs e) {
+          String eredmeny = $"{txtAszam.Text} * {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) * Convert.ToDouble(txtBszam.Text)}";
+          MessageBox.Show("A szorzás eredménye:" + eredmeny);
+          lbEredmenyek.Items.Add(eredmeny);
+        }
+
+        private void btnOszt_Click(object sender, RoutedEventArgs e) {
+          if((Convert.ToDouble(txtBszam.Text) ==0)) {
+            MessageBox.Show("0-val nem osztunk!");
+          } else {
+          String eredmeny = $"{txtAszam.Text} / {txtBszam.Text} = {Convert.ToDouble(txtAszam.Text) / Convert.ToDouble(txtBszam.Text)}";
+          MessageBox.Show("Az osztás eredménye:" + eredmeny);
+          lbEredmenyek.Items.Add(eredmeny);
+          }
+        }
+    }
+}
